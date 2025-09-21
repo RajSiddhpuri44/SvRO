5221a4c647593875da352bf0317293902ff66ac6
Raj Siddhpuri <rajsiddhpuri38@gmail.com>
2025-09-21 21:11:24 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/db');
+const express = require("express");
+// const cors = require('cors');
+// require("dotenv").config();
+// const connectDB = require('./config/db');
 
-const clientRoutes = require('./routes/client.routes');
-const serviceRoutes = require('./routes/service.routes');
-
+// const clientRoutes = require('./routes/client.routes');
+// const serviceRoutes = require('./routes/service.routes');
 
 // Connect to Database
-connectDB();
+// connectDB();
 
 const app = express();
 
 // Middleware
-app.use(cors()); 
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 // A simple test route
-app.get('/', (req, res) => {
-  res.send('RO Service CRM API is running...');
+app.get("/", (req, res) => {
+  res.send("RO Service CRM API is running...");
 });
 
-app.use('/api/clients', clientRoutes);
-app.use('/api/services', serviceRoutes); 
+// app.use('/api/clients', clientRoutes);
+// app.use('/api/services', serviceRoutes);
 
 const PORT = process.env.PORT || 5001;
 
